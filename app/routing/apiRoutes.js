866bb9e218ef9ca4d22737f40fe86570b89ac7bb
Cody Brannon <codybrannon@lawn-143-215-89-116.lawn.gatech.edu>
2017-04-22 11:14:10 -0400

    Added package.json

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,3 +3,18 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
 const fs = require("fs");
+
+module.exports = function(app) {
+    // Add this line below
+    app.use(bodyParser.urlencoded({ extended: true })) 
+
+    app.use(bodyParser.json());
+
+    app.get('/api/tables', function (req, res) {
+        res.sendFile(path.join(__dirname, '../data', 'reservations.js'));
+    });
+
+    app.post('/api/tables', function (req, res) {
+        console.log("Tables");
+    });
+}
