d250aa48d377d216e469f1474cfed9b112e5be5d
Cody Brannon <codybrannon@lawn-143-215-89-116.lawn.gatech.edu>
2017-04-22 11:06:41 -0400

    Default html route

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -16,4 +16,8 @@ module.exports = function(app) {
     app.get('/reserve', function (req, res) {
         res.sendFile(path.join(__dirname, '../public', 'reserve.html'));
     });
+
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, '../public', 'home.html'));
+    });
 }
