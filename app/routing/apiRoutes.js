7967f5a31cc56379a8e303f858112f0760a4eddc
Cody Brannon <codybrannon@lawn-143-215-89-116.lawn.gatech.edu>
2017-04-22 12:43:48 -0400

    api routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -15,6 +15,20 @@ module.exports = function(app) {
     app.use(bodyParser.json());
 
     app.get('/api/tables', function (req, res) {
+        var reservations = [
+            {
+                "name": "Jimi Hendrix",
+                "phone_number": 1234567890,
+                "email": "jimi@areyouexperienced.com",
+                "reservation_type": "reservation"
+            },
+            {
+                "name": "Person1",
+                "phone_number": 1510395713,
+                "email": "himi@areyo.com",
+                "reservation_type": "wait-list"
+            }
+        ];
         // res.sendFile(path.join(__dirname, '../data', 'reservations.js'));
         console.log("Get Tables");
         res.json(reservations);
@@ -22,12 +36,28 @@ module.exports = function(app) {
     });
 
     app.post('/api/tables', function (req, res) {
-        console.log(req.body);
-        res.end();
+        var wait_list = {
+                "reservation_type": "wait-list"
+        };
+
+        var reservation = {
+                "reservation_type": "reservation"
+        };
+
+        var randNum = Math.floor(Math.random() * 2);
+
+        if (randNum) {
+            res.json(reservation);
+        } else {
+            res.json(wait_list);
+        }
     });
 
     app.delete('/api/tables', function (req, res) {
-        console.log("Delete Tables");
-        res.end();
+        var deleted = {
+                "result": "Success"
+        };
+        console.log(deleted.result);
+        res.json(deleted);
     });
 }
