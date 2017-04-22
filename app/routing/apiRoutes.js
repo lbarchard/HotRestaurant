d987dccc89bedfb018cb080ec91043d65c12829a
Cody Brannon <codybrannon@lawn-143-215-89-116.lawn.gatech.edu>
2017-04-22 12:25:28 -0400

    Reservations api

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,18 +3,31 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
 const fs = require("fs");
+const mysql = require("mysql");
+
+
 
 module.exports = function(app) {
+    require('../data/reservations.js');
     // Add this line below
     app.use(bodyParser.urlencoded({ extended: true })) 
 
     app.use(bodyParser.json());
 
     app.get('/api/tables', function (req, res) {
-        res.sendFile(path.join(__dirname, '../data', 'reservations.js'));
+        // res.sendFile(path.join(__dirname, '../data', 'reservations.js'));
+        console.log("Get Tables");
+        res.json(reservations);
+        //
     });
 
     app.post('/api/tables', function (req, res) {
-        console.log("Tables");
+        console.log(req.body);
+        res.end();
+    });
+
+    app.delete('/api/tables', function (req, res) {
+        console.log("Delete Tables");
+        res.end();
     });
 }
